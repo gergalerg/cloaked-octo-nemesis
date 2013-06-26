1a3213550ca35c94ce22853f00198b115b015958
Greg Russell <gregspace@gmail.com>
2013-06-25 20:45:53 -0700

    Listing 4.4 	modified:   .gitignore 	deleted:    arith.cpp 	modified:   arrayone.cpp 	deleted:    assign_st.cpp 	deleted:    bmi.cpp 	deleted:    bondini.cpp 	deleted:    bondini.s 	deleted:    carrots.cpp 	deleted:    chartype.cpp 	deleted:    convert.cpp 	deleted:    divide.cpp 	deleted:    exceed.cpp 	deleted:    floatnum.cpp 	deleted:    fltadd.cpp 	deleted:    getinfo.cpp 	deleted:    height.cpp 	deleted:    hexoct1.cpp 	deleted:    hexoct2.cpp 	deleted:    init.cpp 	modified:   instr1.cpp 	deleted:    instr2.cpp 	deleted:    instr3.cpp 	deleted:    lat.cpp 	deleted:    limits.cpp 	deleted:    limits1.cpp 	deleted:    modulus.cpp 	deleted:    morechar.cpp 	deleted:    myfirst.cpp 	deleted:    numstr.cpp 	deleted:    ourfunc.cpp 	deleted:    ourfunc1.cpp 	deleted:    sqrt.cpp 	modified:   strings.cpp 	deleted:    strtype1.cpp 	deleted:    strtype2.cpp 	deleted:    strtype3.cpp 	deleted:    strtype4.cpp 	deleted:    structur.cpp 	deleted:    test.cpp 	deleted:    typecast.cpp

--- arrayone.cpp
+++ arrayone.cpp
@@ -1,44 +1,36 @@
-// arrayone.cpp == small arrays of integers
+// arrayone.cpp -- small arrays of integers
 #include <iostream>
 int main()
 {
-	using namespace std;
-	int yams[10]; 	// creates an array with three elements
-	yams[0] = 7;
-	yams[1] = 8;
-	yams[2] = 6;
+    using namespace std;
+    short yams[3];        // creates an array with 3 elements
+    yams[0] = 7;
+    yams[1] = 8;
+    yams[2] = 6;
 
-	int beets[1];
-	beets[0] = 5;
+    short yamcosts[3] = {20, 30, 5}; // create, initialize array
 
-	int yamcosts[3] = {20, 30, 5}; // create, initialize array
+    double earnings[4] =  {1.2e4, 1.6e4, 1.7e4};
+    cout << "Size of earnings array: " << sizeof earnings << endl;
+    cout << earnings[0] << " " << earnings[1] << " " << earnings[2] << endl;
 
-	cout << "Total yams = ";
-	cout << yams[0] + yams[1] + yams[2] << endl;
-	cout << "Total beets = ";
-	cout << beets[0] + beets[1] + beets[2] << endl;
-	cout << "The package wth " << yams[1] << " yams costs ";
-	cout << yamcosts[1] << " cents per yam.\n";
-	int total = yams[0] * yamcosts[0] + yams[1] * yamcosts[1];
-	total = total + yams[2] * yamcosts[2];
-	cout << "The total yam expense is " << total << " cents.\n";
+    cout << "Total yams = ";
+    cout << yams[0] + yams[1] + yams[2] << endl;
+    cout << "The package with " << yams[1] << " yams costs ";
+    cout << yamcosts[1] << " cents per yam \n";
+    int total = yams[0] * yamcosts[0] + yams[1] * yamcosts[1];
+    total += yams[2] * yamcosts[2];
+    cout << "The total yam expense " << total << " cents.\n";
 
-	cout << "\nSize of yams array = " << sizeof yams;
-	cout << " bytes.\n";
-	cout << "\nSize of beets array = " << sizeof beets;
-	cout << " bytes.\n";
-	cout << "Size of one element = " << sizeof yams[0];
-	cout << " bytes.\n";
-	cout << "Size of one element beets  = " << sizeof beets[0];
-	cout << " bytes.\n";
-
-	short things[] = {1,5,3,8};
-	int num_elements = sizeof things / sizeof (short);
-	cout << num_elements << endl;
-	cout << "sizeof things " << sizeof things << endl;
-	cout << "sizeof (short) " << sizeof (short) << endl;
-
-	double plifs[] = {25, 92, 3.3};
-	cout << sizeof plifs[0] << " " << plifs[2] << endl;
-	return 0;
+    cout << "\nSize of yams array = " << sizeof yams;
+    cout << " bytes\n";
+    cout << "Size of one element = "  << sizeof yams[0];
+    cout << " bytes.\n";
+    char dog[8] = {'f', 'a', 't', 'e', 's', 's', 'a', '\0'};
+    char dog1[8] = {'f', 'a', 't', 'e', 's', 's', 'a'};
+    cout << "dog: " << dog << "sizeof: dog " << sizeof dog << endl;
+    cout << "sizeof dog1: " << sizeof dog1 << endl;
+    char shirt_size = 'S';
+    cout << "Shirt_size: " << shirt_size << endl;
+    return 0;
 }
--- instr1.cpp
+++ instr1.cpp
@@ -1,17 +1,17 @@
-// instr1.cpp == reading more than one string
+// instrl.cpp reading more than one string
 #include <iostream>
 int main()
 {
-	using namespace std;
-	const int ArSize = 20;
-	char name[ArSize];
-	char dessert[ArSize];
+    using namespace std;
+    const int ArSize  = 20;
+    char name[ArSize];
+    char dessert[ArSize];
 
-	cout << "Enter your name: \n";
-	cin >> name;
-	cout << "Enter your favorite dessert:\n";
-	cin >> dessert;
-	cout << "I have some delicious " << dessert;
-	cout << " for you, " << name << ".\n";
-	return 0;
+    cout << "Enter your name:\n";
+    cin.getline(name, 20); 
+    cout << " Enter your favorite dessert:\n";
+    cin.getline(dessert,30);
+    cout << "I have some delicious " << dessert;
+    cout << " for you, " << name << ".\n";
+    return 0;
 }
