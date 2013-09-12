8b1a59af813f87daf5b89e3f496e65ac2b140b38
Greg Russell <gregspace@gmail.com>
2013-09-12 15:08:25 -0700

    inc dec

---
+++ plus_one.cpp
@@ -0,0 +1,17 @@
+// plus_one.cpp
+#include <iostream>
+int main()
+{
+	using std::cout;
+	using std::endl;
+	int a = 20;
+	int b = 20;
+
+	cout << ++a << endl;
+	cout << a << endl;
+	cout << b++ << endl;
+	cout << b << endl;
+
+
+	return 0;
+}
