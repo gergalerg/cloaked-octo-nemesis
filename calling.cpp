3ac3230231fba9c9fc3087b02fa490d2dff58f32
Greg Russell <gregspace@gmail.com>
2013-10-31 14:08:18 -0700

    Added function prototype

--- calling.cpp
+++ calling.cpp
@@ -1,23 +1,22 @@
 // calling.cpp -- defining, prototyping, and calling a function
 #include <iostream>
 
-void simple(); // function prototype
+void simple(int n); // function prototype
 
 int main()
 {
 	using namespace std;
 	cout << "main() will call the simple() function:\n";
-	simple();   // function call
+	simple(8);   // function call
 	cout << "Main() is finished with the simple() function.\n";
 	// cin.get();
 	return 0;
 }
 
 // function definition
-void simple()
+void simple(int n)
 {
 	using namespace std;
-	int n = 8;
 	for (int i = 0; i < n; i++)
 		cout << "CHeers!\n";
 		cout << endl;
